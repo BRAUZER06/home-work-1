4ba416398639dfa5010913d415f5dc5e284e00da
AHMATOLOG <murtaz.oziev@mail.ru>
2021-11-24 17:05:48 +0300

    w

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/tutorial-1/App';
+import App from '../../../JS/tutorial-1/App.js.js';
 
 
 ReactDOM.render(
