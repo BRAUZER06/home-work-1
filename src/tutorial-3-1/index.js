6b8bbbdcdcf446331e132652bdb1c78d482cc83f
AHMATOLOG <murtaz.oziev@mail.ru>
2021-12-05 11:25:49 +0300

    добавил функцилнал для отображеия кол задач и подправил код

--- src/tutorial-3-1/index.js
+++ src/tutorial-3-1/index.js
@@ -1,46 +1,58 @@
+let input = document.querySelector("#input");
+let addTask = document.querySelector("#addBtn");
+let tasks_div = document.querySelector("#tasks");
+let span = document.querySelector(".span");
 
-let input = document.querySelector('#input');
-let addTask = document.querySelector('#addBtn');
-let tasks_div = document.querySelector('#tasks');
+let tasks = [];
+// let tasksTrackDown =[]
 
-let tasks = [
-  { text: 'Купить пиццу', completed: true },
-  { text: '12', completed: true },
-];
+// for(let i =0; i<tasks.length;i++){
+//   if(tasks.length === tasksTrackDown.length){
 
-function handleClickAdd() {
-  tasks_div.innerHTML = '';
+//   }
+// }
+
+function render() {
+  tasks_div.innerHTML = "";
 
   tasks.forEach((element, i) => {
-    tasks_div.innerHTML += `<div class="task ${element.completed && 'completed'}">
+    tasks_div.innerHTML += `<div class="task ${
+      element.completed && "completed"
+    }">
     ${element.text}
     <div class="bnts">
-      <input id='${i}'  type="checkbox" ${element.completed && 'checked'}/>
+      <input id='${i}'  type="checkbox" ${element.completed && "checked"}/>
       <img id='${i}delete-btn' class="delete-btn" src="https://img.icons8.com/office/16/000000/delete-sign.png" />
     </div>
   </div>`;
-  });  
-  
-
+  });
 }
 
-handleClickAdd();
+function numbersTask() {
+  span.innerHTML = `Всего задач: ${tasks.length}`;
+}
 
-addTask.addEventListener('click', () => {
-  tasks.push({ text:  input.value, completed: false });
-  handleClickAdd();
+addTask.addEventListener("click", () => {
+  tasks.push({ text: input.value, completed: false });
+  if (input.value == "") {
+    alert("Поле пустое");
+    tasks.pop(); //я знаю что можно сделать по-другому. Но хотелось бы пока-что оставить это, и реализовать другой функцинал ) p.s который я кста не реализовал ))
+  }
+  input.value = "";
+  render();
+
+  numbersTask();
+  // let tasksTrackDown = tasks.slice()
+  // console.log(tasksTrackDown);
 });
 
-tasks_div.addEventListener('click', (e) => {
-  if (e.target.type === 'checkbox') {
+tasks_div.addEventListener("click", (e) => {
+  if (e.target.type === "checkbox") {
     tasks[e.target.id].completed = !tasks[e.target.id].completed;
-  } else if (e.target.className === 'delete-btn') {
+  } else if (e.target.className === "delete-btn") {
     console.log(e.target.id[0]);
     tasks.splice(e.target.id[0], 1);
   }
-  handleClickAdd();
+  render();
+  numbersTask();
 });
-
-
-
-
