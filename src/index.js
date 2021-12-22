476b7049dc7cb696759002c5c8ff255307cc5133
AHMATOLOG <murtaz.oziev@mail.ru>
2021-12-22 15:26:55 +0300

    взял копию верстки с сайта и сделал через условный рендеринг* отображении window.location.pathname в отдельном блоке

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-  // import theme from './theme'
-  // import {ThemeProvider} from '@mui/material/styles'
 
-
-
-// import App from './tutorial-1/App.js';
-// import App from './tutorial-2/App.js'
-// import App from './tutorial-4/App'
-
-// import App from './tutorial-5/App'
-// import App from "./tutorial-5-1/App";
 import App from "./tutorial-6/App";
+import './index.css'
+import 'bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
-// import App from './tutorial-5-2/App'
-import "./index.css";
-
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
-      <App />
-    {/* </ThemeProvider> */}
-  </React.StrictMode>,
-  document.getElementById("root")
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
 );
-
--- src/tutorial-6/Article.jsx
+++ src/tutorial-6/Article.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { hashHistory } from 'react-router';
+
 const Article = ({id}) => {
   return (
     <div>
